cfe62b077b8804cbd39369adbaeb8c9e3b1d91a7
aletsire <aletsire@gmail.com>
2021-11-25 11:26:02 +0900

    user

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,4 +7,4 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = get_user_model()
-        fields = ('username', 'password', 'email')
+        fields = ('username', 'password', 'email', 'id')
