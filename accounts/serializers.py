0294d68f5970e1c47518910e6092eb182c2df1d3
aletsire <aletsire@gmail.com>
2021-11-25 11:54:25 +0900

    like modified

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    email = serializers.CharField(write_only=True)
     
     
     class Meta:
